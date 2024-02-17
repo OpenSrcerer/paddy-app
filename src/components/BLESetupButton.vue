6507336a205859e6874568019288f182f09a5146
Daniel Stefani <daniel.stefani@crispybacon.al>
2024-02-17 17:59:07 +0200

    feat: add simple functionality to pass wifi ssid/pass to pads

---
+++ src/components/BLESetupButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <button type="button" @click="emit('setupBLE')">
+      Inject Credentials
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+const emit = defineEmits(["setupBLE"])
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,42 +1,35 @@
 <template>
   <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
+    <div id="container">
+      <label>Wifi SSID:</label>
+      <input type="text" v-model="ssidForm"><br><br>
+
+      <label>Wifi Password:</label>
+      <input type="text" v-model="passForm"><br><br>
+
+      <div>
+        <BLESetupButton @setupBLE="injectCredentials"/>
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script setup lang="ts">
-import { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
+import BLESetupButton from 'components/BLESetupButton.vue';
 import { ref } from 'vue';
+import { sendSsidAndPass } from 'src/bluetooth/BLEService';
 
-const todos = ref<Todo[]>([
-  {
-    id: 1,
-    content: 'ct1'
-  },
-  {
-    id: 2,
-    content: 'ct2'
-  },
-  {
-    id: 3,
-    content: 'ct3'
-  },
-  {
-    id: 4,
-    content: 'ct4'
-  },
-  {
-    id: 5,
-    content: 'ct5'
-  }
-]);
-const meta = ref<Meta>({
-  totalCount: 1200
-});
+const ssidForm = ref()
+const passForm = ref()
+
+const injectCredentials = () => {
+  sendSsidAndPass(ssidForm.value, passForm.value)
+}
 </script>
+
+<style scoped lang="scss">
+#id {
+  display: flex;
+  flex-direction: column;
+}
+</style>
